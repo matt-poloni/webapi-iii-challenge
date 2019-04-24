6c89b0391e5700f239bdcaad538a3afd6c2af97e
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-24 18:41:20 -0400

    Creates validUserID middleware for posts POST & PUT

--- routes/posts.js
+++ routes/posts.js
@@ -17,19 +17,24 @@ const hasUserID = (req, res, next) => {
     : next();
 }
 
+const validUserID = async (req, res, next) => {
+  const invalid = await dbUsers.getById(req.body.user_id);
+  !invalid
+    ? res.status(400).json({ error: 'Please provide a valid user_id.' })
+    : next();
+}
+
 router.get('/', (req, res) => {
   db.get()
     .then(users => res.status(200).json(users))
     .catch(err => res.status(500).json({ error: 'The posts data could not be retrieved.' }))
 })
 
-router.post('/', hasText, hasUserID, (req, res) => {
+router.post('/', hasText, hasUserID, validUserID, (req, res) => {
   const newPost = req.body;
-  !dbUsers.getById(newPost.user_id)
-    ? res.status(400).json({ error: 'Please provide a valid user_id.' })
-    : db.insert(newPost)
-        .then(inserted => res.status(201).json(inserted))
-        .catch(err => res.status(500).json({ error: 'There was an error while creating the new post.' }))
+  db.insert(newPost)
+    .then(inserted => res.status(201).json(inserted))
+    .catch(err => res.status(500).json({ error: 'There was an error while creating the new post.' }))
 })
 
 router.get('/:id', (req, res) => {
@@ -43,20 +48,17 @@ router.get('/:id', (req, res) => {
     .catch(err => res.status(500).json({ error: 'The post information could not be retrieved' }))
 })
 
-router.put('/:id', hasText, hasUserID, (req, res) => {
+router.put('/:id', hasText, hasUserID, validUserID, (req, res) => {
   const newPost = req.body;
   const postID = req.params.id;
-  !dbUsers.getById(newPost.user_id)
-    ? res.status(400).json({ error: 'Please provide a valid user_id.' })
-    : db.update(postID, newPost)
-      .then(async count => {
-        console.log(count)
-        const updated = await db.getById(postID);
-        !count
-          ? res.status(404).json({ error: 'The post with the specified ID does not exist.' })
-          : res.status(200).json(updated);
-      })
-      .catch(err => res.status(500).json({ error: 'The post information could not be modified.' }))
+  db.update(postID, newPost)
+    .then(async count => {
+      const updated = await db.getById(postID);
+      !count
+        ? res.status(404).json({ error: 'The post with the specified ID does not exist.' })
+        : res.status(200).json(updated);
+    })
+    .catch(err => res.status(500).json({ error: 'The post information could not be modified.' }))
 })
 
 router.delete('/:id', async (req, res) => {
