cd18985d566f1426c44f6985c0dfee07af4f4d1a
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-24 15:24:19 -0400

    Initial set up of routes

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
 
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send("It's working");
+})
+
+server.use('/api/users', usersRouter);
+server.use('/api/posts', postsRouter);
 
 module.exports = server;
