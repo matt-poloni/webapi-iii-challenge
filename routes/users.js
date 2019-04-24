ae6c4364267d1c4c87f7d03374720c7285227f45
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-24 19:08:05 -0400

    Removed unnecessary check for name in nameUpperCase now that hasName exists

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,8 @@ const hasName = (req, res, next) => {
 }
 
 const nameUpperCase = (req, res, next) => {
-  if(req.body.name) {
-    req.body.name = req.body.name.toUpperCase();
-  }
+  // Best used after hasName
+  req.body.name = req.body.name.toUpperCase();
   next();
 }
 
