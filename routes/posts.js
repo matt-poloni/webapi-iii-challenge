e3e8a68581465cd732241f7799d1c1301d11c450
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-24 15:31:25 -0400

    Adds simple GET to users and posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -4,4 +4,10 @@ const db = require('../data/helpers/postDb');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  db.get()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ error: 'The posts data could not be retrieved.' }))
+})
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,10 @@ const db = require('../data/helpers/userDb');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  db.get()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ error: 'The users data could not be retrieved.' }))
+})
+
 module.exports = router;
